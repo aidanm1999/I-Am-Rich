eb8b1114865dcfaa9557e9c676934b2f6e42a590
Aidan Marshall <aidanmarshall1999@gmail.com>
2020-04-04 20:27:08 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aidan Marshall on 04/04/2020.
+//  Copyright © 2020 Aidan Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
